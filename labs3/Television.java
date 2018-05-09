0a47fc6ba3ef5564166c8aec794c253353e61eae
MvHoorn88 <m.hoorn88@gmail.com>
2018-05-09 22:39:30 +0200

    getter setter en toString

--- labs3/Television.java
+++ labs3/Television.java
@@ -6,15 +6,28 @@ package labs3;
 public class Television {
 
 	// INSTANCE VARIABLES
-	String brand = "RCA"; // brand name (with initializer)
-	int volume; // volume (with no initializer)
+	private String brand; // brand name (with initializer)
+	private int volume; // volume (with no initializer)
 
 	// ACCESSOR METHODS
-	public String getBrand() {
+	public void setBrand(String brandIn) {   //SETTER
+		brand = brandIn;
+	}
+
+	public String getBrand() {               // GETTER
 		return brand;
 	}
 
-	public int getVolume() {
+	public void setVolume(int volumeIn) {   // SETTER
+		volume = volumeIn;
+	}
+
+	public int getVolume() {                // GETTER
 		return volume;
+	}
+
+	// returns a string representation of this class instance
+	public String toString() {
+		return "Television: brand=" + this.getBrand() + ", volume=" + this.getVolume();
 	}
 }
--- labs3/TelevisionTest.java
+++ labs3/TelevisionTest.java
@@ -6,14 +6,21 @@ public class TelevisionTest {
  * Class with main method,where the program STARTS
  */
 	public static void main(String[] args) {
+		
 	// INSTANTIATE two Televisions
 		Television tv01 = new Television();
 		Television tv02 = new Television();
 		
-    // assign value to the brand using direct acces
-		tv01.brand = "Hitachi";
-		System.out.println("Television: brand=" + tv01.getBrand() + ", volume=" +tv01.getVolume());
-		System.out.println("television: brand=" + tv02.getBrand() + ", volume=" + tv02.getVolume());
+    // assign value to the brands and volumes using setter methods
+		tv01.setBrand("Hitachi");
+		tv01.setVolume(15);
+		
+		tv02.setBrand("Sony");
+		tv02.setVolume(40);
+		
+		// displays the values using the toString() method
+		System.out.println(tv01);   // implicit toString call
+		System.out.println(tv02.toString()); //explicit toString call
 	}
 
 }
